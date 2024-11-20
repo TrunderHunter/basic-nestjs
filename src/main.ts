afece7d62b10a9a55ad945b349d97d616b1b8185
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2024-11-20 10:16:55 +0700

    add DTOs for likes, menus, orders, reviews, menu items, and restaurants; create modules for likes, menus, orders, reviews, menu items, and restaurants; update app service and tsconfig; add example .env file

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
   await app.listen(port);
 }
 bootstrap();
